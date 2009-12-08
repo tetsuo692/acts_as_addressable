3235333b12c54dbe375a6dc8af23983aff0924c2
guillaume belleguic <guillaume.belleguic@cixi.eu>
2009-12-08 13:25:42 +0100

    add addressable item association for acts_as_addressable

--- lib/acts_as_addressable.rb
+++ lib/acts_as_addressable.rb
@@ -1,6 +1,10 @@
 module ActsAsAddressable
   module ClassMethods
     def acts_as_addressable(options = {})
+      options = {:sort_column => :title}.merge(options)
+      
+      has_one :addressable_item, :class_name => "AddressableItem", :as => :addressable
+      
       send :include, InstanceMethods
     end
   end
