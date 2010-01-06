b51981c763ea3818eb3afe5f5bcf49d32c236b15
guillaume belleguic <guillaume@MacBook-Pro-de-guillaume-belleguic.local>
2010-01-06 16:48:59 +0100

    add method missing

--- lib/app/models/addressable_item.rb
+++ lib/app/models/addressable_item.rb
@@ -2,4 +2,7 @@ class AddressableItem < ActiveRecord::Base
   belongs_to :addressable, :polymorphic => true
   has_many :addresses, :class_name => "Address"
   
+  def method_missing(method_name, *args)
+    self.addressable.send(method_name, *args) if self.addressable.respond_to?(method_name)
+  end
 end
